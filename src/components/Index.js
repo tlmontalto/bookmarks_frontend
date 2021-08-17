a68f027e5e1e8d49ba01a095491caad4769b5374
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-08-17 13:52:41 -0400

    tried adding edit function, it didn't work out

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,9 +3,13 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3003/'
 
+let showForm = false
+
 export default function Index() {
 
     const [bookmarks, setBookmarks] = useState([])
+    const [title, setTitle] = useState('')
+    const [url, setUrl] = useState('')
 
     useEffect(() => {
         async function fetchData() {
@@ -15,21 +19,10 @@ export default function Index() {
         fetchData()
     })
 
-    // const deleteBookmark = async (ev) => {
-    //     // ev.preventDefault()
-    //     try {
-    //         await axios.delete(baseURL + 'bookmark/')
-    //     }
-    //     catch (err) {
-    //         console.log(err)
-    //     }
-    // }
     function deleteBookmark(id) {
         // console.log(id)
         const newBookmarks = bookmarks.filter((bookmark) => bookmark.id !== id)
-
         setBookmarks(newBookmarks)
-
         try {
             axios.delete(`${baseURL}bookmark/${id}`)
         }
@@ -38,14 +31,44 @@ export default function Index() {
         }
     }
 
+    const toggleForm = () => {
+        showForm = !showForm
+        console.log(showForm)
+    }
+
+    function updateBookmark(id) {
+        id.preventDefault()
+        const newBookmarks = bookmarks.filter((bookmark) => bookmark.id !== id)
+        setBookmarks(newBookmarks)
+        try {
+            axios.patch(`${baseURL}bookmark/${id}`)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return (
         <div>
             {bookmarks.map((bookmark) => (
                 <div key={bookmark._id}>
                     <h3>{bookmark.title}</h3>
                     <h3><a href={'http://' + bookmark.url} target='_blank'>{bookmark.url}</a></h3>
-                    {/* <button onClick={deleteBookmark}>DELETE</button> */}
                     <button onClick={() => deleteBookmark(bookmark._id)}>DELETE</button>
+                    <button onClick={toggleForm}>EDIT</button>
+
+                <form onSubmit={updateBookmark}>
+                    <label htmlFor="title" >Title: </label>
+                    <input onChange={(ev) => setTitle(ev.target.value)} type="text" id="title" name="title" defaultValue={bookmark.title} required/>
+
+                    <label htmlFor="url">URL: </label>
+                    <input onChange={(ev) => setUrl(ev.target.value)} type="text" id="url" name="url" defaultValue={bookmark.url} required/>
+
+                    <button type="submit">Update</button>
+                </form>
+
                 </div>
             ))}
         </div>
