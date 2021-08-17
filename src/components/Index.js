89b3017e4963ab9e9a25788e121f95603d5b68bd
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-08-17 12:00:41 -0400

    delete button on index page works

--- src/components/Index.js
+++ src/components/Index.js
@@ -15,12 +15,37 @@ export default function Index() {
         fetchData()
     })
 
+    // const deleteBookmark = async (ev) => {
+    //     // ev.preventDefault()
+    //     try {
+    //         await axios.delete(baseURL + 'bookmark/')
+    //     }
+    //     catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+    function handleRemove(id) {
+        // console.log(id)
+        const newBookmarks = bookmarks.filter((bookmark) => bookmark.id !== id)
+
+        setBookmarks(newBookmarks)
+
+        try {
+            axios.delete(`${baseURL}bookmark/${id}`)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
             {bookmarks.map((bookmark) => (
                 <div key={bookmark._id}>
                     <h3>{bookmark.title}</h3>
                     <h3><a href={'http://' + bookmark.url} target='_blank'>{bookmark.url}</a></h3>
+                    {/* <button onClick={deleteBookmark}>DELETE</button> */}
+                    <button onClick={() => handleRemove(bookmark._id)}>DELETE</button>
                 </div>
             ))}
         </div>
