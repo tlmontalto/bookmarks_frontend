99dce69882228d7e68f10287c437e8c4e8126890
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-08-17 12:09:19 -0400

    delete is clean

--- src/components/Index.js
+++ src/components/Index.js
@@ -24,7 +24,7 @@ export default function Index() {
     //         console.log(err)
     //     }
     // }
-    function handleRemove(id) {
+    function deleteBookmark(id) {
         // console.log(id)
         const newBookmarks = bookmarks.filter((bookmark) => bookmark.id !== id)
 
@@ -45,7 +45,7 @@ export default function Index() {
                     <h3>{bookmark.title}</h3>
                     <h3><a href={'http://' + bookmark.url} target='_blank'>{bookmark.url}</a></h3>
                     {/* <button onClick={deleteBookmark}>DELETE</button> */}
-                    <button onClick={() => handleRemove(bookmark._id)}>DELETE</button>
+                    <button onClick={() => deleteBookmark(bookmark._id)}>DELETE</button>
                 </div>
             ))}
         </div>
